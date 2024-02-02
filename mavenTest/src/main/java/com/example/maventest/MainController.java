e4213aa4637cb418d4ff631d2d791862c9045c25
kang1211 <zdxsca1211@naver.com>
2024-02-02 19:26:28 +0900

    ssssss

---
+++ mavenTest/src/main/java/com/example/maventest/MainController.java
@@ -0,0 +1,101 @@
+package com.example.maventest;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.maventest.DTO.memberDTO;
+import com.example.maventest.service.MemberService;
+
+@Controller
+public class MainController {
+	private final MemberService memberService;
+	
+	public  MainController(MemberService memberService) {
+		this.memberService=memberService;
+	}
+	
+	@GetMapping("/")
+	public ModelAndView main(HttpSession session) {
+		ModelAndView mv = new ModelAndView("index");
+		//session.invalidate();
+		return mv;
+	}
+	
+	@GetMapping("/signup")
+	public ModelAndView signup() {
+		ModelAndView mv = new ModelAndView("signup");
+		return mv;
+	}
+	
+	@GetMapping("/signin")
+	public ModelAndView signin() {
+		ModelAndView mv = new ModelAndView("signin");
+		return mv;
+	}
+	@GetMapping("/update")
+	public ModelAndView update() {
+		ModelAndView mv = new ModelAndView("update");
+		return mv;
+	}
+	
+	@PostMapping("/signin")
+	public ModelAndView signin(@RequestParam Map<String, String> param, 
+				HttpSession session) {
+		ModelAndView mv = new ModelAndView("index");
+		
+		/* System.out.println(param.get("email")); */
+		String name = memberService.login(param);
+		//mv.addObject("user",name);
+		if(name != null)
+			session.setAttribute("user", name);
+		else {
+			mv.setViewName("signin");
+			mv.addObject("fail", "a");
+		}
+		return mv;
+	}
+	
+	@PostMapping("/signup/Enroll")
+	public ModelAndView signEnroll(@ModelAttribute memberDTO memberdto) {
+		
+		ModelAndView mv = new ModelAndView("index");
+		System.out.println(memberdto.getEmail());
+		memberService.insert(memberdto);
+		return mv;
+	}
+	
+	@PostMapping("/update/Enroll")
+	public ModelAndView updateEnroll(@ModelAttribute memberDTO memberdto) {
+		
+		ModelAndView mv = new ModelAndView("index");
+		memberService.change(memberdto);
+		return mv;
+	}
+	
+
+/*
+ * @PostMapping("/update/User") public ModelAndView updateUser(@ModelAttribute
+ * memberDTO memberdto) { ModelAndView mv = new ModelAndView("update");
+ * memberService.insert(memberdto); return mv; }
+ */
+	
+//	public ModelAndView Enroll(
+//			@RequestParam("email") String email, 
+//			@RequestParam("pin") String pw, 
+//			@RequestParam("name") String name, 
+//			@RequestParam("tel") String tel) {
+//		
+//		ModelAndView mv = new ModelAndView("index");
+//		System.out.println(email);
+//		
+//		return mv;
+//	}
+}
---
+++ mavenTest/src/main/webapp/WEB-INF/views/signin.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> 로그인 페이지 </title>
+</head>
+<body>
+	
+	<form method="post" action="/signin">
+		이메일 : <input type="email" name="email"> <br>
+		비밀번호 : <input type="password" name="pin"> <br>
+		<button>로그인</button>
+	</form>
+</body>
+</html>
